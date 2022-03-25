2eff62a7dffb525c2786ade190e3007062b98b3c
JoyWang <yihan.wang3@ucalgary.ca>
2022-03-24 21:06:24 -0600

    Update main.cpp

--- AL_CARPONE/main.cpp
+++ AL_CARPONE/main.cpp
@@ -17,6 +17,52 @@ struct Character {
 
 std::map<char, Character> Characters;
 
+unsigned int VAO, VBO;
+
+void RenderText(GraphicsSystem* s, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+	// activate corresponding render state	
+	s->shader2D->use();
+	glUniform3f(glGetUniformLocation(s->shader2D->ID, "textColor"), color.x, color.y, color.z);
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(VAO);
+
+	// iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = Characters[*c];
+
+		float xpos = x + ch.Bearing.x * scale;
+		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		float w = ch.Size.x * scale;
+		float h = ch.Size.y * scale;
+		// update VBO for each character
+		float vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos,     ypos,       0.0f, 1.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+			{ xpos + w, ypos + h,   1.0f, 0.0f }
+		};
+		// render glyph texture over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// update content of VBO memory
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 int main()
 {
 	// TODO clean up
@@ -80,6 +126,7 @@ int main()
 	//////////////////
 	//test free type//
 	//////////////////
+	
 	// FreeType
 	// --------
 	FT_Library ft;
@@ -150,6 +197,18 @@ int main()
 	//////////////////
 	//test free type//
 	//////////////////
+
+	// configure VAO/VBO for texture quads
+	// -----------------------------------
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	// Initialize Models
 	player.createModel(); //TODO: If player is moved here as well, we can create model in constructors instead.
@@ -224,7 +283,7 @@ int main()
 				state.activeVehicles.pop_back();
 			}
 		
-			
+			RenderText(&graphics, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
 			// If exiting the main menu
 			if (state.gamestate == GAMESTATE_INGAME) {
 
@@ -407,11 +466,14 @@ int main()
 
 			renderAll(activeCamera, &graphics, &mainMenu, &player, &ui,  &state, &police_car1);
 
+			
+
 			// DEBUG MODE
 			if (state.debugMode) { // Camera is deactivated
 				debugPanel.draw(player);
 			}
 		}
+		
 		graphics.swapBuffers();
 	}
 
