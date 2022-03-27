fcd008e5ee8da14c87b4db336677b0b79d16d739
Sebastian Collard <bbxtnation@gmail.com>
2022-03-27 00:43:03 -0600

    pretty sure this fixes camera bug when robbing the bank...

--- AL_CARPONE/main.cpp
+++ AL_CARPONE/main.cpp
@@ -457,9 +457,6 @@ int main()
 
 			//printf("JAILTIMER: %.2f\n", player.jailTimer);
 
-			//Check for special inputs (currently only camera mode change)
-			checkSpecialInputs(graphics.window, state, player, &audio);
-
 			if (state.cameraMode == CAMERA_MODE_BOUND) activeCamera = &boundCamera;
 			else if (state.cameraMode == CAMERA_MODE_UNBOUND_FREELOOK) activeCamera = &freeCamera;
 
@@ -467,6 +464,9 @@ int main()
 			if (!state.debugMode) activeCamera->handleInput(graphics.window, state);
 			if (activeCamera == &boundCamera) boundCamera.checkClipping(graphics.window);
 
+			//Check for special inputs (currently only camera mode change)
+			// NOTE: needs to be put AFTER camera mode and input checking to prevent camera glitches on certain inputs
+			checkSpecialInputs(graphics.window, state, player, &audio);
 
 			//Check if player has thrown an item (used a tomato or donut powerup)
 			checkForItemActions(&player, &boundCamera, &physics, &state);
